d1987a9a8d4da92729b62938aa604bea038006bb
Shravankumar <shravankumarsayz@gmail.com>
2024-10-23 08:33:44 +0530

    Update and rename halcyon.sh to bbp.sh

---
+++ bbp.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Removals
+rm -rf .repo/local_manifests
+
+# Initialize repo with specified manifest
+repo init -u https://github.com/2by2-Project/android.git -b 14 --git-lfs
+
+# Clone local_manifests repository
+git clone https://github.com/shravansayz/local_manifests.git --depth 1 -b crdroid  .repo/local_manifests
+
+# Sync the repositories
+/opt/crave/resync.sh
+
+#Private Keys
+rm -rf vendor/2by2-priv
+git clone https://github.com/shravansayz/private_keys.git -b rise vendor/2by2-priv
+
+export BUILD_USERNAME=shravan
+export BUILD_HOSTNAME=crave
+
+#build
+source build/envsetup.sh
+lunch lineage_RMX1901-ap2a-user
+m installclean
+m bacon
