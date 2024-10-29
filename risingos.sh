dd6c7a3212fb045b03b20497362757dd87654340
Shravankumar <shravankumarsayz@gmail.com>
2024-10-29 09:46:48 +0530

    Create risingos.sh

---
+++ risingos.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Removals
+rm -rf .repo/local_manifests
+
+# Initialize repo with specified manifest
+repo init -u https://github.com/RisingTechOSS/android -b fifteen --git-lfs
+
+# Clone local_manifests repository
+git clone https://github.com/shravansayz/local_manifests --depth 1 -b rise .repo/local_manifests
+
+# Sync the repositories
+/opt/crave/resync.sh
+
+#Private Keys
+rm -rf vendor/lineage-priv
+git clone https://github.com/shravansayz/private_keys.git -b rise vendor/lineage-priv
+
+export BUILD_USERNAME=shravan
+export BUILD_HOSTNAME=crave
+
+#build
+source build/envsetup.sh
+riseup RMX1901 user
+m installclean
+rise b
