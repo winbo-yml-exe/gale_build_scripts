8de3430e3a73b94e00b7e958153becd2f3f851ba
Shravankumar <shravankumarsayz@gmail.com>
2024-11-04 12:52:21 +0530

    Update derpify.sh

--- derpify.sh
+++ derpify.sh
@@ -17,15 +17,12 @@ export BUILD_HOSTNAME=crave
 
 #customs
 rm -rf frameworks/base
-git clone https://github.com/shravansayz/frameworks_base_derp.git -b 15 frameworks/base --depth=1
+git clone https://github.com/kaderbava/frameworks_base.git -b 15 frameworks/base --depth=1
 
 # packages_apps_Settings
 rm -rf packages/apps/Settings
-git clone -b 15 https://github.com/shravansayz/packages_apps_Settings.git packages/apps/Settings
+git clone -b 15 https://github.com/kaderbava/packages_apps_Settings.git packages/apps/Settings
 
-# audio-hal
-rm -rf hardware/qcom-caf/sdm845/audio
-git clone -b 15 https://github.com/Krtonia/android_hardware_qcom_audio.git hardware/qcom-caf/sdm845/audio
 
 #Private Keys
 rm -rf vendor/derp/signing
@@ -33,6 +30,6 @@ git clone https://github.com/shravansayz/private_keys.git -b rise vendor/derp/si
 
 # Set up build environment
 source build/envsetup.sh
-lunch derp_RMX1901-userdebug
+lunch derp_RMX1901-user
 make installclean
 mka derp
