263242173e8a4f78a96e7744e71bedbaa75a3858
Shravankumar <shravankumarsayz@gmail.com>
2024-11-17 07:27:30 +0530

    Update evox.sh

--- evox.sh
+++ evox.sh
@@ -2,10 +2,9 @@
 
 # Removals
 rm -rf .repo/local_manifests
-rm -rf vendor/evolution-priv
 
 # Initialize repo with specified manifest
-repo init -u https://github.com/Evolution-XYZ/manifest -b udc --git-lfs --depth=1
+repo init -u https://github.com/Evolution-X/manifest -b vic --git-lfs --depth=1
 
 # Clone local_manifests repository
 git clone https://github.com/shravansayz/local_manifests --depth 1 -b evox .repo/local_manifests
@@ -15,17 +14,17 @@ git clone https://github.com/shravansayz/local_manifests --depth 1 -b evox .repo
 
 #customs
 rm -rf frameworks/base
-git clone https://github.com/shravansayz/frameworks_base_evox.git -b udc frameworks/base --depth=1
+git clone https://github.com/shravansayz/frameworks_base_evox.git -b vic frameworks/base --depth=1
 
 # Private Keys
 rm -rf vendor/evolution-priv
-git clone https://github.com/shravansayz/private_keys_evox.git -b rise vendor/evolution-priv
+git clone https://github.com/shravansayz/private_keys.git -b rise vendor/evolution-priv
 
 # Set up build environment
 source build/envsetup.sh
 
 # Lunch configuration
-lunch lineage_RMX1901-user
+lunch lineage_RMX1901-ap3a-user
 
 # Cleanup directories
 make installclean
