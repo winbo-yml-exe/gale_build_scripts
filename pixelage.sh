2a6073c4ccd285085694d3e5051dff628934b429
Shravankumar <shravankumarsayz@gmail.com>
2024-11-26 08:50:03 +0530

    pixelage.sh

---
+++ pixelage.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Removals
+rm -rf .repo/local_manifests
+
+# Initialize repo with specified manifest
+repo init -u https://github.com/ProjectPixelage/android_manifest.git -b 15 --git-lfs
+
+# Clone local_manifests repository
+git clone https://github.com/shravansayz/local_manifests --depth 1 -b pixelage .repo/local_manifests
+
+# Sync the repositories
+/opt/crave/resync.sh
+
+#Private Keys
+rm -rf vendor/lineage-priv
+git clone https://github.com/shravansayz/private_keys.git -b rise vendor/lineage-priv
+
+export PIXELAGE_BUILD=RMX1901
+export BUILD_USERNAME=shravan
+export BUILD_HOSTNAME=crave
+
+#build
+source build/envsetup.sh
+lunch pixelage_RMX1901-ap3a-user
+mka installclean
+mka bacon
