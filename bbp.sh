dcc790b53af1f79c5885d16d52ad6f57952a519c
MohamedDevvv <aaamohamed499@gmail.com>
2024-11-28 16:03:30 +0200

    Update bbp.sh

--- bbp.sh
+++ bbp.sh
@@ -7,7 +7,7 @@ rm -rf .repo/local_manifests
 repo init -u https://github.com/2by2-Project/android.git -b 14 --git-lfs
 
 # Clone local_manifests repository
-git clone https://github.com/shravansayz/local_manifests.git --depth 1 -b crdroid  .repo/local_manifests
+git clone https://github.com/MohamedDevvv/Build-.git --depth 1 -b main  .repo/local_manifests
 
 # Sync the repositories
 /opt/crave/resync.sh
@@ -16,11 +16,11 @@ git clone https://github.com/shravansayz/local_manifests.git --depth 1 -b crdroi
 rm -rf vendor/2by2-priv
 git clone https://github.com/shravansayz/private_keys.git -b rise vendor/2by2-priv
 
-export BUILD_USERNAME=shravan
+export BUILD_USERNAME=Mohamed 👾
 export BUILD_HOSTNAME=crave
 
 #build
 source build/envsetup.sh
-lunch lineage_RMX1901-ap2a-user
+lunch lineage_gale-ap2a-user
 m installclean
 m bacon
