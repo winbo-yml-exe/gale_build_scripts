5a6a48e2212a5380c9a2773553490d1125e0803c
winbo.yml <winbo.yml@proton.me>
2024-12-01 16:46:13 +0100

    Update afterlife.sh

--- afterlife.sh
+++ afterlife.sh
@@ -12,18 +12,14 @@ git clone https://github.com/shravansayz/local_manifests --depth 1 -b afterlife 
 # Sync the repositories
 /opt/crave/resync.sh
 
-#customs
-rm -rf frameworks/base
-git clone https://github.com/shravansayz/frameworks_base_afterlife.git -b 14 frameworks/base --depth=1
-
 #Private Keys
 rm -rf vendor/lineage-priv
 git clone https://github.com/shravansayz/private_keys.git -b rise vendor/after-priv
 
-export BUILD_USERNAME=shravan
+export BUILD_USERNAME=winbo-yml
 export BUILD_HOSTNAME=crave
 
 #build
 . build/envsetup.sh
-m installclean
-goafterlife RMX1901
+make clean
+goafterlife gale
